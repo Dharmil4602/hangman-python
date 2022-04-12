0f5d3088e0ef8a0d20f8d7c2539bdb8bae060a25
Dharmil Shah <dharmilshah476@gmail.com>
2022-04-12 18:02:13 +0530

    Created the starting template

---
+++ hangman.py
@@ -0,0 +1,4 @@
+name = input("Enter Your Name: \n")
+print("Welcome",name, ",Let's Start The Game")
+print("-------------------------------")
+print("So the game is to guess the word within 10 tries")
