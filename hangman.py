88ef9300e9d8bc70933cb3c160d7e8ef4066ebf9
Dharmil Shah <dharmilshah476@gmail.com>
2022-07-20 01:02:44 +0530

    Hangman game completed

--- hangman.py
+++ hangman.py
@@ -1,6 +1,5 @@
 ## Lets create a predefined list inside a function
-from random import random
-from wsgiref.util import guess_scheme
+import random
 
 def hangman():
     ## random.choice will select random word from
@@ -12,6 +11,7 @@ def hangman():
     while len(word) > 0:
         main = ""
         missed = 0
+
         for letter in word:
             if letter in guessMade:
                 main = main + letter
@@ -26,13 +26,72 @@ def hangman():
 
     
     # Checking that the user entered a valid letter
-    if guess in validLetters:
-        guessMade = guessMade + guess
-    else:
-        print("Please enter a valid letter.")
-        guess = input()
-    if guess not in word:
-        turns = turns - 1
+        if guess in validLetters:
+            guessMade = guessMade + guess
+        else:
+            print("Please enter a valid letter.")
+            guess = input()
+        
+        if guess not in word:
+            turns = turns - 1
+            if turns == 9:
+                print("9 turns left")
+                print("----------")
+            if turns == 8:
+                print("8 turns left")
+                print("---------")
+                print("    O   ")
+            if turns == 7:
+                print("7 turns left")
+                print("---------")
+                print("    O   ")
+                print("    |   ")
+            if turns == 6:
+                print("6 turns left")
+                print("---------")
+                print("    O   ")
+                print("    |   ")
+                print("   /   ")
+            if turns == 5:
+                print("5 turns left")
+                print("---------")
+                print("    O   ")
+                print("    |   ")
+                print("   / \  ")
+            if turns == 4:
+                print("4 turns left")
+                print("---------")
+                print("  \ O   ")
+                print("    |   ")
+                print("   / \  ")
+            if turns == 3:
+                print("3 turns left")
+                print("---------")
+                print("  \ O /  ")
+                print("    |   ")
+                print("   / \  ")
+            if turns == 2:
+                print("2 turns left")
+                print("---------")
+                print("  \ O /|  ")
+                print("    |   ")
+                print("   / \  ")
+            if turns == 1:
+                print("1 turns left")
+                print("---------")
+                print("  \ O _|/  ")
+                print("    |   ")
+                print("   / \  ")
+            if turns == 0:
+                print("You Loose")
+                print("---------")
+                print("    O _|  ")
+                print("   /|\   ")
+                print("   / \  ")
+                break
+
+
+
 
 name = input("Enter Your Name: \n")
 print("Welcome",name, ",Let's Start The Game")
